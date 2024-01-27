fca2676174d715845ff83fc491c0e7b8ea3ad5eb
mjshouf <156518630+mjshouf@users.noreply.github.com>
2024-01-27 00:22:17 -0800

    Add files via upload

---
+++ reversedStrings.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+
+using namespace std;                 //although code ran, the "problems" list grew
+                                     //so added "namespace std" to clean that up
+int main() {
+    cout << "Enter a word: ";        // ask user to input a single word
+    string w{};	
+    cin >> w;
+    int n = w.length();              //length of word for calculation
+    
+    for (int i = 0; i < n / 2; i++)  //characters are swapped to halfway of length
+{
+        swap(w[i], w[n - i - 1]);    //loop until length less than half of length
+    }
+
+    cout << "Reversed string: " << w << endl;
+    return 0;
+}
