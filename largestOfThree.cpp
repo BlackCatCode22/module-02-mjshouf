b5ed4ef987f8ac895eadeb36fedc567db6031162
mjshouf <156518630+mjshouf@users.noreply.github.com>
2024-01-31 12:41:52 -0800

    Update largestOfThree.cpp

--- largestOfThree.cpp
+++ largestOfThree.cpp
@@ -1,16 +1,16 @@
 #include <iostream>
 using namespace std;
 
-int main() {						// Program to return the highest of three user input
-									// integers using nested if-else statements
+int main() {					// Program to return the highest of three user input
+						// integers using nested if-else statements
 
-	int answer;						// declare variabe to store the largest integer
-	int num1; int num2; int num3;	// variables to store user input
+	int answer;				// declare variabe to store the largest integer
+	int num1; int num2; int num3;		// variables to store user input
 
 	cout << "Enter three integers: " << '\n';
     	cin >> num1 >> num2 >> num3;
 
-	  if (num1 >= num2)				// compare the first and second variable
+	  if (num1 >= num2)			// compare the first and second variable
  	   {
 	        if (num1 >= num3)		// compare the first and third variable
 	        {
@@ -32,8 +32,8 @@ int main() {						// Program to return the highest of three user input
 	            answer = num3;		// the third variable is the highest integer
 	        }
 	    }
-	
-	cout << "The largest integer is " << answer << endl; // display largest integer
+						// display largest integer
+	cout << "The largest integer is " << answer << endl; 	
 	return 0;
 }
 
